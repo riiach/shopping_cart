06092a0e8689b29b83c1466a4b088866a5e4c673
riiach <riiachoii@gmail.com>
2025-12-03 14:43:55 +0900

    port change

--- server/index.js
+++ server/index.js
@@ -33,5 +33,6 @@ app.use((req, res) => {
     res.status(404).json({ message: "Route not found" });
 });
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(process.env.PORT, () =>
+    console.log(`Server running on port ${process.env.PORT}`)
+);
